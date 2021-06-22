6a2be0200974a1ff6657e9f9a29c7faac66d33c2
Andrew Reynolds <andrew_reynolds@mac.com>
2021-06-22 15:03:18 -0700

    learning how to pass data up using functions from parent component

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = ({ onSaveExpenseData }) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
   const [enteredExpense, setEnteredExpense] = useState("");
@@ -21,7 +21,7 @@ const ExpenseForm = () => {
       amount: enteredExpense,
       date: new Date(enteredDate),
     };
-    console.log(expenseData);
+    onSaveExpenseData(expenseData);
     setEnteredTitle("");
     setEnteredDate("");
     setEnteredExpense("");
