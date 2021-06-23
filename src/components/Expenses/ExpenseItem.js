3268de1b3c8736ce58010a6ee1998e34aeff99e8
Andrew Reynolds <andrew_reynolds@mac.com>
2021-06-23 00:25:10 -0700

    removed change title button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,23 +1,15 @@
-import { useState } from "react";
-
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = ({ title, amount, date }) => {
-  const [h2Title, setH2Title] = useState(title);
-  const clickHandler = () => {
-    setH2Title("Updated");
-    console.log(h2Title);
-  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{h2Title}</h2>
+        <h2>title</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
